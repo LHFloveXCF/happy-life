d7bc50d5010365e26340898b4e1f0e7047bb0cf9
xuchaofeng <xuchaofeng@yoozoo.com>
2024-11-14 10:51:44 +0800

    home-pageTitle添加每日诗词

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,26 @@
 import './index.scss';
 
 import { siteTitle } from '@/utils/constant';
-import { useTitle } from 'ahooks';
+import { useTitle, useMount, useSafeState } from 'ahooks';
 import PageTitle from '@/components/PageTitle';
 import Section from './Section';
 
-
-const getPoem = require('jinrishici');
+const getPoem = require('jinrishici')
 
 function Home() {
     useTitle(siteTitle);
+    const [poem, setPoem] = useSafeState('');
+
+    useMount(() => {
+        getPoem.load(data => {
+            console.log('输出诗句内容', data.data.content);
+            setPoem(data.data.content)
+        })
+    })
 
     return (
         <>
-            <PageTitle title={siteTitle} desc={''} className={"homeTitle"} />
+            <PageTitle title={siteTitle} desc={poem || ''} className={"homeTitle"} />
             <div className={"body"}>
                 <Section />
             </div>
