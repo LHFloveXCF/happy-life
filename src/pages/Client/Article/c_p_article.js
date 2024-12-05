87eb18857fee06f551418edab171de3606a09c5d
LHFloveXCF <826639991@qq.com>
2024-12-05 22:36:40 +0800

    后台页面：提交文章5

--- src/pages/Client/Article/c_p_article.js
+++ src/pages/Client/Article/c_p_article.js
@@ -3,6 +3,10 @@ import MarkDown from "@/components/MarkDown";
 import useUrlState from '@ahooksjs/use-url-state';
 import { useSelector } from "react-redux";
 
+import style from './index.module.scss';
+import g from '@/global.custom.scss';
+import classNames from "classnames";
+
 function ClientArticle() {
 
     const [search] = useUrlState();
@@ -11,15 +15,16 @@ function ClientArticle() {
 
     const article = homeState.article_info_list.find(item => item.id === Number(search.title));
 
-
-    console.log("search: ", article);
-
+    console.log("s ", style);
+    
 
     return (
         <>
-            <Layout>
-                <MarkDown content={article.content}></MarkDown>
-            </Layout>
+            <div className={style.c_p_article_show_father}>
+                <div>title</div>
+                <div><MarkDown content={article.content}></MarkDown></div>
+                <div>opreate</div>
+            </div>
 
         </>
     );
