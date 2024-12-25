1fb99cc5cc0c48238c58fd23c380d12bbe3e9f76
xuchaofeng <xuchaofeng@yoozoo.com>
2024-12-25 19:57:03 +0800

    首页：增加说说界面2

--- src/pages/Client/Say/c_p_say.js
+++ src/pages/Client/Say/c_p_say.js
@@ -1,19 +1,32 @@
 // 说说展示界面
 // 打赏弹出框
-import style from './index.module.scss';
-import '../../../global.custom.scss';
-import classNames from "classnames";
 import { imgUrlPrefix } from '@/utils/constant';
-import { useState } from 'react';
+import classNames from "classnames";
+import '../../../global.custom.scss';
+import SaySayContent from './c_p_say_content';
+import style from './index.module.scss';
 
 function SaySay({ isModalOpen, handleOk, handleCancel }) {
-    const [isExpanded, setIsExpanded] = useState(false);
-
     const says = [
         {
             "img": `${imgUrlPrefix}1733380822066.webp`,
             "content": "content-mid",
             "userName": "content-mid",
+            "userId": 1,
+            "replay": [
+                {
+                    "img": `${imgUrlPrefix}1733380822066.webp`,
+                    "content": "content-mid",
+                    "userName": "content-mid",
+                }
+            ],
+
+        },
+        {
+            "img": `${imgUrlPrefix}1733380822066.webp`,
+            "content": "content-mid",
+            "userName": "content-mid",
+            "userId": 1,
             "replay": [
                 {
                     "img": `${imgUrlPrefix}1733380822066.webp`,
@@ -33,53 +46,12 @@ function SaySay({ isModalOpen, handleOk, handleCancel }) {
                 </div>
                 <div className={classNames(style.s_mid)}>
                     <div className={classNames(style.s_content_home)}>
-                        {says.length !== 0 && says.map((item, index) => (<div className={classNames("all_card", style.s_content_main)}>
-                            <div className={classNames(style.s_content_l)}>
-                                <img src={item.img} className={classNames(style.s_content_img)} alt=''></img>
-                            </div>
-                            <div className={classNames(style.s_content_mid)}>
-                                <div className={classNames(style.s_c_detail_home)}>
-                                    <div className={classNames(style.s_c_detail_mid)}>
-                                        {item.content}
-                                    </div>
-                                    {item.replay.length !== 0 && item.replay.map((item, index) => (
-                                        <div className={classNames(style.s_c_detail_footer)}>
-                                            <div className={classNames(style.s_content_l)}>
-                                                <img src={item.img} className={classNames(style.s_c_detail_img)} alt=''></img>
-                                            </div>
-                                            <div className={classNames(style.s_c_detail_r)} >
-                                                {item.content}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                                {/** 回复区域 */}
-                                {isExpanded ? (
-                                    <div className={classNames(style.s_content_footer)}>
-                                        <textarea
-                                            value=""
-                                            // onChange={handleReplyChange}
-                                            className={classNames(style.s_c_replay_expanded_input)}
-                                        />
-                                        <div className={classNames(style.s_c_replay_home)}>
-                                            <button className={classNames(style.reply_button)} onClick={() => setIsExpanded(false)}>回复</button>
-                                        </div>
-                                    </div>
-                                ) : (
-                                    <div className={classNames(style.s_content_footer)}>
-                                        <input
-                                            type="text"
-                                            onClick={() => setIsExpanded(true)}
-                                            placeholder="写下你的回复..."
-                                            className={classNames(style.s_c_replay_collapsed_input)}
-                                        />
-                                    </div>
-                                )}
-                            </div>
-                        </div>))}
+                        {says.length !== 0 && says.map((item, index) => (
+                            <SaySayContent item={item}></SaySayContent>
+                        ))}
                     </div>
                 </div>
-                <div className={classNames(style.bordered_div)}>
+                <div>
                     footer
                 </div>
             </div>
---
+++ src/pages/Client/Say/c_p_say_content.js
@@ -0,0 +1,66 @@
+
+import classNames from "classnames";
+import { useState } from 'react';
+import style from './index.module.scss';
+import '../../../global.custom.scss';
+
+function SaySayContent({ item }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [userId, setUserId] = useState(0);
+    const [replay, setReplay] = useState("");
+
+    const handleExpanded = (userId, isExpanded) => {
+        setIsExpanded(isExpanded);
+        setUserId(userId);
+    };
+    return (
+        <>
+            <div className={classNames("all_card", style.s_content_main)}>
+                <div className={classNames(style.s_content_l)}>
+                    <img src={item.img} className={classNames(style.s_content_img)} alt=''></img>
+                </div>
+                <div className={classNames(style.s_content_mid)}>
+                    <div className={classNames(style.s_c_detail_home)}>
+                        <div className={classNames(style.s_c_detail_mid)}>
+                            {item.content}
+                        </div>
+                        {item.replay.length !== 0 && item.replay.map((item, index) => (
+                            <div className={classNames(style.s_c_detail_footer)}>
+                                <div className={classNames(style.s_c_detail_l)}>
+                                    <img src={item.img} className={classNames(style.s_c_detail_img)} alt=''></img>
+                                </div>
+                                <div className={classNames(style.s_c_detail_r)} >
+                                    {item.content}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    {/** 回复区域 */}
+                    {isExpanded && userId == item.userId ? (
+                        <div className={classNames(style.s_content_footer)}>
+                            <textarea
+                                value={replay}
+                                onChange={(e) => setReplay(e.target.value)}
+                                className={classNames(style.s_c_replay_expanded_input)}
+                            />
+                            <div className={classNames(style.s_c_replay_home)}>
+                                <button className={classNames(style.reply_button)} onClick={() => handleExpanded(item.userId, false)}>回复</button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className={classNames(style.s_content_footer)}>
+                            <input
+                                type="text"
+                                onClick={() => handleExpanded(item.userId, true)}
+                                placeholder="写下你的回复..."
+                                className={classNames(style.s_c_replay_collapsed_input)}
+                            />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SaySayContent;
